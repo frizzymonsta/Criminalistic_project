af7b7ae0556db19588c662ca8e12cb2fc3a194e0
frizzymonsta <Pe4eHbkO.o@gmail.com>
2021-01-20 13:53:53 +0300

    [+] Comments added.

--- main.py
+++ main.py
@@ -1,14 +1,17 @@
 import json
 
+# Импорт логов событий Windows и алертов Surikata
 general = 'suricata_alert.json'
 events = 'WinEventLog.json'
 
 
+# Функция, выгружающая данные из json файлов, в т.ч. указатели.
 def file_to_json(file_name):
     with open(file_name, 'r') as file:
         return json.loads(file.read())
 
 
+# Скан файлов по времени на наличие соответствий.
 def map_to_entries(output, json, sublist_name):
     for item in json:
         time = item['_time']
@@ -22,13 +25,18 @@ def map_to_entries(output, json, sublist_name):
         output[time][sublist_name].append(item)
 
 
+# Вызов функции file_to_json.
 general_data = file_to_json(general)
 events_data = file_to_json(events)
 
+
+# Вызов функции map_to_entries, запись всех событий в единый словарь entries по меткам 'g' и 'e'.
 entries = {}
 map_to_entries(entries, general_data, 'g')
 map_to_entries(entries, events_data, 'e')
 
+
+# Выявление пар алертов и событий, внесение их в массив collision.
 collision = []
 
 for entry in entries:
@@ -37,5 +45,7 @@ for entry in entries:
     if 'g' in item and 'e' in item:
         collision.append(item)
 
+
+# Запись в файл вывода.
 with open('output.json', 'w') as out_file:
     out_file.write(json.dumps(collision, indent=2))
